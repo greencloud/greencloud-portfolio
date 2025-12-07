4bf447845529a5c99d97b74ad82b5b68dbd905ba
Greencloud <Greencloud>
2025-12-08 01:32:09 +0800

    fix: patch google tag manager in the <head>

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,17 +1,20 @@
 export default function Head() {
   return (
     <>
-      {/* GTM script MUST be in <head> */}
+      {/* GA4 gtag.js snippet */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-MNLJ711DQ8"
+      ></script>
+
       <script
         dangerouslySetInnerHTML={{
           __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM_ID}'+dl;
-            f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
-          `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MNLJ711DQ8');
+          `,
         }}
       />
     </>
