4838a345cd7def22e44d4f4c8fa77a962c9acef9
Greencloud <Greencloud>
2025-12-08 01:27:48 +0800

    fix: add custom header for GTM integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,17 +30,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
 
-        {/* GTM Script */}
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM_ID}'+dl;
-            f.parentNode.insertBefore(j,f);
-          `}
-        </Script>
-
         {/* GTM NoScript */}
         <noscript>
           <iframe
