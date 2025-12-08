9fd3b6695c0dccc3ddaef935bb8c715446e7246f
Greencloud <Greencloud>
2025-12-09 02:58:42 +0800

    feat: add opengraph feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,26 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "My Online Portfolio | Garry Molleno Jr.",
   description: "Explore the portfolio of Garry Molleno Jr., a web developer specializing in custom dynamic web-based applications. Discover expertise in modern web technologies, recent projects, and professional digital solutions tailored for businesses of any scale.",
-  keywords: "Garry Molleno Jr, web developer, portfolio, custom web applications, dynamic websites, digital solutions, full-stack development, front-end development, back-end development, skill set, recent projects, hire web developer"
+  keywords: "Garry Molleno Jr, web developer, portfolio, custom web applications, dynamic websites, digital solutions, full-stack development, front-end development, back-end development, skill set, recent projects, hire web developer",
+  openGraph: {
+    title: "Garry Molleno Jr. &ndash; Portfolio",
+    description: "Explore the portfolio of Garry Molleno Jr., a web developer specializing in custom dynamic web-based applications. Discover expertise in modern web technologies, recent projects, and professional digital solutions tailored for businesses of any scale.",
+    url: "https://portfolio.sectorzlabs.com/",
+    siteName: "My Online Portfolio | Garry Molleno Jr.",
+    images: [
+      {
+        url: "/opengraph.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Portfolio Open Graph Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
+
+
 
 export default function RootLayout({
   children,
